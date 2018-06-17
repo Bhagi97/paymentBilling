58daf180664187d1b2b4f080dde511218973eae8
Bhagi97 <bhagirathiramesh.mec@gmail.com>
2018-06-17 23:26:54 +0530

    commit-5

--- paymentBilling/ElaAdmin/urls.py
+++ paymentBilling/ElaAdmin/urls.py
@@ -2,12 +2,12 @@ from django.conf.urls import url, include
 from . import views
 
 urlpatterns = [
-    url(r'^', views.index),  # home page
+
     url(r'^index.html', views.index),
     url(r'^exportCustomer.html', views.exportCustomer),
     url(r'^form_submit_Customer/', views.add_details_Customer),
     url(r'^form_submit_Invoice/', views.add_details_Invoice),
-    url(r'^page-error-400.html', views.page_error)
-
+    url(r'^page-error-400.html', views.page_error),
+    url(r'^', views.index)  # home page
     # path('form_submit_Invoice/', views.add_details_Invoice),
 ]
